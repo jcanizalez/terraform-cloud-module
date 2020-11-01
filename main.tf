17f3c751f4ec2204a8aaf155e8118a7be8d65280
Javier Canizalez <javier-canizalez@outlook.com>
2020-11-01 02:25:24 +0000

    Include use module

---
+++ main.tf
@@ -0,0 +1,14 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.34.0"
+  features {}
+}
+
+module "full-webapp" {
+  source  = "app.terraform.io/devops-mindset/full-webapp/azurerm"
+  version = "0.0.1"
+  name = "Fuel"
+  location = "eastus"
+  # insert required variables here
+}
